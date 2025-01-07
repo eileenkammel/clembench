e2981647674ad2c128673df445c1383ad4c2d883
Eileen <eniedenfuehr@gmail.com>
2025-01-07 16:26:16 +0100

    add working, separate programmatic player

---
+++ games/multimodal_referencegame_comprehension/prep/make_tuna_stimuli.py
@@ -0,0 +1,332 @@
+# -*-coding:utf-8 -*-
+# Author: Eileen Kammel, 811770
+# Date: 2024-10-22 17:58:59
+
+# Pairs a target with matchin distractors. Works for 2 or 3 distractors.
+
+
+import json
+import random
+import itertools
+
+# set seed for reproducibility
+random.seed(1012)
+
+
+def define_id(n):
+    # Return all possible combinations of n attributes
+    # by attribut list indices
+    attribute_list = [0, 1, 2, 3]
+    ids = itertools.combinations(attribute_list, n)
+    return list(ids)
+
+
+def extract_attributes(image):
+    # Return a list of all attributes of an image
+    return [
+        image["ATTRIBUTE"]["@COLOUR"],
+        image["ATTRIBUTE"]["@ORIENTATION"],
+        image["ATTRIBUTE"]["@TYPE"],
+        image["ATTRIBUTE"]["@SIZE"],
+    ]
+
+
+def determine_id_style(id):
+    id_style = {
+        (0,): "colour",
+        (1,): "orientation",
+        (2,): "type",
+        (3,): "size",
+        (0, 1): "colour_orientation",
+        (0, 2): "colour_type",
+        (0, 3): "colour_size",
+        (1, 2): "orientation_type",
+        (1, 3): "orientation_size",
+        (2, 3): "type_size",
+        (0, 1, 2): "colour_orientation_type",
+        (0, 1, 3): "colour_orientation_size",
+        (0, 2, 3): "colour_type_size",
+        (1, 2, 3): "orientation_type_size",
+    }
+    return id_style[id]
+
+
+def return_rndm_distractor(distractor_count, excluded_imgs):
+    all_img = range(0, 97)
+    possible_distractors = list(set(all_img) - set(excluded_imgs))
+    distractors = random.sample(possible_distractors, distractor_count)
+    distractors = [f"{distractor}.png" for distractor in distractors]
+    return distractors
+
+
+def get_image_numbers(images):
+    return [image.strip(".png") for image in images]
+
+
+def find_distractors(target, target_attributes, target_id, distractor_count):
+    # Return two distractors that fit the target_id
+    all_img = json.load(
+        open("games/multimodal_referencegame/prep/tuna/all_imgs.json", "r")
+    )
+    attribute_idx = set([0, 1, 2, 3])
+    target_id = set(target_id)
+    if len(target_id) == 1:
+        possible_distractors = []
+        # attributes that are not in the target_id by idx
+        non_id_attributes = list(attribute_idx.difference(target_id))
+        target_id = list(target_id)
+        # index of the target attribute(s)
+        a1_idx = target_id[0]
+        # loop over all images and find distractors
+        for image in all_img["IMAGES"]:
+            image_filename = image["@IMAGE"]
+            # get the attributes of the potential distractor
+            image_attributes = extract_attributes(image)
+            # skip img which have same attribute(s) as target id
+            if image_attributes[a1_idx] == target_attributes[a1_idx]:
+                continue
+            # rest of the attributes must be the same
+            else:
+                if (
+                    image_attributes[non_id_attributes[0]]
+                    == target_attributes[non_id_attributes[0]]
+                    and image_attributes[non_id_attributes[1]]
+                    == target_attributes[non_id_attributes[1]]
+                    and image_attributes[non_id_attributes[2]]
+                    == target_attributes[non_id_attributes[2]]
+                ):
+                    possible_distractors.append(image_filename)
+        # return two random distractors if they exist, else skip
+        try:
+            possible_distractors = random.sample(possible_distractors, 2)
+            if distractor_count == 2:
+                return possible_distractors
+            elif distractor_count == 3:
+                imgs_nums = get_image_numbers(possible_distractors + [target])
+                additional_distractors = return_rndm_distractor(1, imgs_nums)
+            return possible_distractors + additional_distractors
+        except ValueError:
+            print("Not enough distractors found!")
+            return None
+
+    elif len(target_id) == 2:
+        possible_distractors1 = []
+        possible_distractors2 = []
+        # attributes that are not in the target_id by idx
+        non_id_attributes = list(attribute_idx.difference(target_id))
+        target_id = list(target_id)
+        # index of the target attribute(s)
+        a1_idx = target_id[0]
+        a2_idx = target_id[1]
+        # loop over all images and find distractors
+        for image in all_img["IMAGES"]:
+            image_filename = image["@IMAGE"]
+            image_attributes = extract_attributes(image)
+            # skip img which have same attribute(s) as target id
+            if (
+                image_attributes[a1_idx] == target_attributes[a1_idx]
+                and image_attributes[a2_idx] == target_attributes[a2_idx]
+            ):
+                continue
+            else:
+                # find first distractor candidates
+                if (
+                    image_attributes[a1_idx] == target_attributes[a1_idx]
+                    and image_attributes[a2_idx] != target_attributes[a2_idx]
+                    and image_attributes[non_id_attributes[0]]
+                    == target_attributes[non_id_attributes[0]]
+                    and image_attributes[non_id_attributes[1]]
+                    == target_attributes[non_id_attributes[1]]
+                ):
+                    possible_distractors1.append(image_filename)
+                # find second distractor candidates
+                elif (
+                    image_attributes[a1_idx] != target_attributes[a1_idx]
+                    and image_attributes[a2_idx] == target_attributes[a2_idx]
+                    and image_attributes[non_id_attributes[0]]
+                    == target_attributes[non_id_attributes[0]]
+                    and image_attributes[non_id_attributes[1]]
+                    == target_attributes[non_id_attributes[1]]
+                ):
+                    possible_distractors2.append(image_filename)
+        # return one random distractor from each list if they exist, else skip
+        try:
+            possible_distractors1 = random.sample(possible_distractors1, 1)
+            possible_distractors2 = random.sample(possible_distractors2, 1)
+            distractors = possible_distractors1 + possible_distractors2
+            if distractor_count == 2:
+                return distractors if len(distractors) == 2 else None
+            elif distractor_count == 3:
+                imgs_nums = get_image_numbers(distractors + [target])
+                additional_distractors = return_rndm_distractor(1, imgs_nums)
+                return distractors + additional_distractors
+        except ValueError:
+            print("Not enough distractors found!")
+            return None
+    elif len(target_id) == 3:
+        possible_distractors1 = []
+        possible_distractors2 = []
+        possible_distractors3 = []
+        # attributes that are not in the target_id by idx
+        non_id_attributes = list(attribute_idx.difference(target_id))
+        target_id = list(target_id)
+        # index of the target attribute(s)
+        a1_idx = target_id[0]
+        a2_idx = target_id[1]
+        a3_idx = target_id[2]
+        # loop over all images and find distractors
+        for image in all_img["IMAGES"]:
+            image_filename = image["@IMAGE"]
+            image_attributes = extract_attributes(image)
+            # skip img which have same attribute(s) as target id
+            if (
+                image_attributes[a1_idx] == target_attributes[a1_idx]
+                and image_attributes[a2_idx] == target_attributes[a2_idx]
+                and image_attributes[a3_idx] == target_attributes[a3_idx]
+            ):
+                continue
+            else:
+                # find first distractor candidates
+                if (
+                    image_attributes[a1_idx] != target_attributes[a1_idx]
+                    and image_attributes[a2_idx] == target_attributes[a2_idx]
+                    and image_attributes[a3_idx] == target_attributes[a3_idx]
+                    and image_attributes[non_id_attributes[0]]
+                    == target_attributes[non_id_attributes[0]]
+                ):
+                    possible_distractors1.append(image_filename)
+                # find second distractor candidates
+                elif (
+                    image_attributes[a1_idx] == target_attributes[a1_idx]
+                    and image_attributes[a2_idx] != target_attributes[a2_idx]
+                    and image_attributes[a3_idx] == target_attributes[a3_idx]
+                    and image_attributes[non_id_attributes[0]]
+                    == target_attributes[non_id_attributes[0]]
+                ):
+                    possible_distractors2.append(image_filename)
+                # find third distractor candidates
+                elif (
+                    image_attributes[a1_idx] == target_attributes[a1_idx]
+                    and image_attributes[a2_idx] == target_attributes[a2_idx]
+                    and image_attributes[a3_idx] != target_attributes[a3_idx]
+                    and image_attributes[non_id_attributes[0]]
+                    == target_attributes[non_id_attributes[0]]
+                ):
+                    possible_distractors3.append(image_filename)
+        # return one random distractor from each list if they exist, else skip
+        try:
+            possible_distractors1 = random.sample(possible_distractors1, 1)
+            possible_distractors2 = random.sample(possible_distractors2, 1)
+            possible_distractors3 = random.sample(possible_distractors3, 1)
+            distractors = (
+                possible_distractors1 + possible_distractors2 + possible_distractors3
+            )
+            return distractors if len(distractors) == 3 else None
+        except ValueError:
+            print("Not enough distractors found!")
+            return None
+
+
+def make_stimuli_sets(distractor_count):
+
+    assert distractor_count in [2, 3], "Only 2 or 3 distractors are allowed!"
+
+    if distractor_count == 2:
+        json_instances = {"INSTANCES": {"one_attibute_id": [], "two_attibute_id": []}}
+    elif distractor_count == 3:
+        json_instances = {
+            "INSTANCES": {
+                "one_attibute_id": [],
+                "two_attibute_id": [],
+                "three_attibute_id": [],
+            }
+        }
+
+    episode_num = 0
+
+    # loop over all_img.json, image will be target
+    all_img = json.load(
+        open("games/multimodal_referencegame/prep/tuna/all_imgs.json", "r")
+    )
+
+    for img in all_img["IMAGES"]:
+        t_filename = img["@IMAGE"]
+        # get the target attributes
+        all_attributes = extract_attributes(img)
+
+        # find ids for target with all
+        # possible lengths & combinations of attributes
+        # max len = 2 if distractors = 2,
+        # max len = 3 if distractors = 3
+        range_end = distractor_count + 1
+        for i in range(1, range_end):
+            # find all possible ids
+            ids = define_id(i)
+            # get distractors for all ids
+            for id in ids:
+
+                id_attributes = [all_attributes[idx] for idx in id]
+                id_attributes = ",".join(id_attributes).strip(",")
+                distractors = find_distractors(
+                    t_filename, all_attributes, id, distractor_count
+                )
+                if distractors is not None:
+                    if distractor_count == 2:
+                        distractor1 = distractors[0]
+                        distractor2 = distractors[1]
+                        episode = {
+                            "stimuli_id": episode_num,
+                            "target": t_filename,
+                            "distractor1": distractor1,
+                            "distractor2": distractor2,
+                            "id_type": determine_id_style(id),
+                            "id_attributes": id_attributes,
+                        }
+                        if i == 1:
+                            json_instances["INSTANCES"]["one_attibute_id"].append(
+                                episode
+                            )
+                        elif i == 2:
+                            json_instances["INSTANCES"]["two_attibute_id"].append(
+                                episode
+                            )
+
+                        episode_num += 1
+                    elif distractor_count == 3:
+                        distractor1 = distractors[0]
+                        distractor2 = distractors[1]
+                        distractor3 = distractors[2]
+                        episode = {
+                            "stimuli_id": episode_num,
+                            "target": t_filename,
+                            "distractor1": distractor1,
+                            "distractor2": distractor2,
+                            "distractor3": distractor3,
+                            "id_type": determine_id_style(id),
+                            "id_attributes": id_attributes,
+                        }
+                        if i == 1:
+                            json_instances["INSTANCES"]["one_attibute_id"].append(
+                                episode
+                            )
+                        elif i == 2:
+                            json_instances["INSTANCES"]["two_attibute_id"].append(
+                                episode
+                            )
+                        elif i == 3:
+                            json_instances["INSTANCES"]["three_attibute_id"].append(
+                                episode
+                            )
+
+                        episode_num += 1
+
+    with open(
+        f"games/multimodal_referencegame/prep/tuna/tuna_{distractor_count}_distractor_stimuli.json",
+        "w",
+    ) as json_file:
+        json.dump(json_instances, json_file, indent=4)
+    print("All instances saved successfully!")
+
+
+if __name__ == "__main__":
+    make_stimuli_sets(distractor_count=3)
---
+++ games/multimodal_referencegame_comprehension/prep/visualize_instances.py
@@ -0,0 +1,122 @@
+# -*-coding:utf-8 -*-
+# Author: Eileen Kammel, 811770
+# Date: 2024-10-23 15:37:48
+
+# Make HTML file of all experiment episodes
+# Add all 4 pictures + id_type
+import os
+import json
+import csv
+import pandas as pd
+from bs4 import BeautifulSoup
+import base64
+
+
+def get_id_type_name(set_name, id):
+    tuna_stimuli = json.load(
+        open("games/multimodal_referencegame/prep/tuna/tuna_3_distractor_stimuli.json")
+    )
+    threeds_stimuli = json.load(
+        open("games/multimodal_referencegame/prep/3DS/3ds_3_distractor_instances.json")
+    )
+    stimuli = tuna_stimuli if set_name == "tuna" else threeds_stimuli
+    for experiment in stimuli["INSTANCES"]:
+        for stimulus in stimuli["INSTANCES"][experiment]:
+            if stimulus["stimuli_id"] == id:
+                type = stimulus["id_type"]
+                attributes = stimulus["id_attributes"]
+                return type, attributes
+
+
+def visualize_instances():
+    # load my instances
+    instances = json.load(
+        open("games/multimodal_referencegame/in/updated_my_instances_3distractors.json")
+    )
+    csv_header = ["Target", "D1", "D2", "D3", "id_type", "id_attributes"]
+    csv_data = []
+
+    for experiment in instances["experiments"]:
+
+        experiment_name = "tuna" if "TUNA" in experiment["name"] else "threeds"
+
+        for episode in experiment["game_instances"]:
+
+            stimuli_id = episode["stimuli_id"]
+
+            if episode["player_1_target_position"] == 1:
+                target = episode["player_1_first_image"]
+                d1 = episode["player_1_second_image"]
+                d2 = episode["player_1_third_image"]
+                d3 = episode["player_1_fourth_image"]
+            elif episode["player_1_target_position"] == 2:
+                d1 = episode["player_1_first_image"]
+                target = episode["player_1_second_image"]
+                d2 = episode["player_1_third_image"]
+                d3 = episode["player_1_fourth_image"]
+            elif episode["player_1_target_position"] == 3:
+                d1 = episode["player_1_first_image"]
+                d2 = episode["player_1_second_image"]
+                target = episode["player_1_third_image"]
+                d3 = episode["player_1_fourth_image"]
+            elif episode["player_1_target_position"] == 4:
+                d1 = episode["player_1_first_image"]
+                d2 = episode["player_1_second_image"]
+                d3 = episode["player_1_third_image"]
+                target = episode["player_1_fourth_image"]
+            id_type, id_ttributes = get_id_type_name(experiment_name, stimuli_id)
+            csv_data.append([target, d1, d2, d3, id_type, id_ttributes])
+    with open("games/multimodal_referencegame/prep/updated_instances.csv", "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(csv_header)
+        writer.writerows(csv_data)
+
+
+def create_html():
+    instances_df = pd.read_csv("games/multimodal_referencegame/prep/updated_instances.csv")
+    instances_html = instances_df.to_html(index=False)
+
+    with open("games/multimodal_referencegame/prep/updated_instances.html", "w") as f:
+        f.write(instances_html)
+    print("HTML table generated")
+
+
+def img_to_base64(img_path):
+    with open(img_path, "rb") as img_file:
+        return base64.b64encode(img_file.read()).decode("utf-8")
+
+
+def create_html_with_img():
+    tuna_path = "/Users/eileen/Desktop/clembench/games/multimodal_referencegame/resources/tuna_images"
+    threeds_path = "/Users/eileen/Desktop/clembench/games/multimodal_referencegame/resources/3ds_images"
+    with open("games/multimodal_referencegame/prep/updated_instances.html", "r") as f:
+        soup = BeautifulSoup(f, "html.parser")
+    table = soup.find("table")
+    for cell in table.find_all("td"):
+        cell_text = cell.get_text().strip()
+        if "/" in cell_text:
+            if "tuna" in cell_text:
+                img_tag = soup.new_tag("img")
+                img = os.path.basename(cell_text)
+                img_path = os.path.join(tuna_path, img)
+                img_base64 = img_to_base64(img_path)
+                img_tag["src"] = f"data:image/png;base64,{img_base64}"
+                cell.clear()
+                cell.append(img_tag)
+            elif "3ds" in cell_text:
+                img_tag = soup.new_tag("img")
+                img = os.path.basename(cell_text)
+                img_path = os.path.join(threeds_path, img)
+                img_base64 = img_to_base64(img_path)
+                img_tag["src"] = f"data:image/png;base64,{img_base64}"
+                cell.clear()
+                cell.append(img_tag)
+    with open("games/multimodal_referencegame/prep/updated_instances_img.html", "w") as f:
+        f.write(str(soup))
+    print("HTML table with images generated")
+
+
+if __name__ == "__main__":
+    visualize_instances()
+    create_html()
+    create_html_with_img()
