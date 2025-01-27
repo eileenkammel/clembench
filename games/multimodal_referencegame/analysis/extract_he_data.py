1b177c4ad628325c9aa71b3b6dc6b3ac432d2258
Eileen <eniedenfuehr@gmail.com>
2025-01-27 17:11:22 +0100

    include target name in fiel

--- games/multimodal_referencegame/analysis/extract_he_data.py
+++ games/multimodal_referencegame/analysis/extract_he_data.py
@@ -15,6 +15,7 @@ def parse_game_log(logfile):
         stimuli_id = None
         clue = None
         correct = None
+        target = None
         for line in game_log:
             line_json = json.loads(line)
             if "pictures" in line_json["event"]:
@@ -26,11 +27,29 @@ def parse_game_log(logfile):
                 stimuli_id = line_json["data"]["content"]
             if line_json["event"] == "clue":
                 clue = line_json["data"]["content"]
-            if line_json["event"] == "correct guess" or line_json["event"] == "false guess":
+            if (
+                line_json["event"] == "correct guess"
+                or line_json["event"] == "false guess"
+            ):
+                target = find_target(int(stimuli_id), game_style[-1])
                 correct = True if line_json["event"] == "correct guess" else False
-                all_data.append({"stimuli_id": int(stimuli_id), "human_expression": clue, "success": correct})
+                all_data.append(
+                    {
+                        "stimuli_id": int(stimuli_id),
+                        "target": target,
+                        "human_expression": clue,
+                        "success": correct,
+                    }
+                )
                 if correct:
-                    only_successful.append({"stimuli_id": int(stimuli_id), "human_expression": clue, "success": correct})
+                    only_successful.append(
+                        {
+                            "stimuli_id": int(stimuli_id),
+                            "target": target,
+                            "human_expression": clue,
+                            "success": correct,
+                        }
+                    )
                 stimuli_id = None
                 clue = None
                 correct = None
@@ -43,9 +62,38 @@ def parse_game_log(logfile):
             if len(all_data) != 9:
                 print("Game was aborted")
         if game_style == "mixed":
-            if len(all_data) !=6:
+            if len(all_data) != 6:
                 print("Game was aborted")
         return game_style, all_data, only_successful
+
+
+def find_target(stimuli_id, game_style):
+    if game_style == "tuna":
+        with open(
+            "games/multimodal_referencegame/prep/tuna/tuna_3_distractor_stimuli.json"
+        ) as f:
+            data = json.load(f)
+            all_stimuli = (
+                data["INSTANCES"]["one_attibute_id"]
+                + data["INSTANCES"]["two_attibute_id"]
+                + data["INSTANCES"]["three_attibute_id"]
+            )
+            for stimuli in all_stimuli:
+                if stimuli["stimuli_id"] == stimuli_id:
+                    return stimuli["target"]
+    if game_style == "3ds":
+        with open(
+            "games/multimodal_referencegame/prep/3DS/3ds_3_distractor_instances.json"
+        ) as f:
+            data = json.load(f)
+            all_stimuli = (
+                data["INSTANCES"]["one_attibute_id"]
+                + data["INSTANCES"]["two_attibute_id"]
+                + data["INSTANCES"]["three_attibute_id"]
+            )
+            for stimuli in all_stimuli:
+                if stimuli["stimuli_id"] == stimuli_id:
+                    return stimuli["target"]
 
 
 def parse_all_logs(log_folder):
@@ -61,7 +109,9 @@ def parse_all_logs(log_folder):
     # make json file from games dict
     with open("games/multimodal_referencegame/analysis/all_human_expressions.json", "w") as f:
         json.dump(all_games, f)
-    with open("games/multimodal_referencegame/analysis/successful_human_expressions.json", "w") as f:
+    with open(
+        "games/multimodal_referencegame/analysis/successful_human_expressions.json", "w"
+    ) as f:
         json.dump(successful_games, f)
     return all_games, successful_games
 
