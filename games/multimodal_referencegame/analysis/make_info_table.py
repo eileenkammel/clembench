8bcefac9d56c27b9e2391de75a3120f97fdf46ac
Eileen <eniedenfuehr@gmail.com>
2025-01-31 12:37:41 +0100

    add to info table

--- games/multimodal_referencegame/analysis/make_info_table.py
+++ games/multimodal_referencegame/analysis/make_info_table.py
@@ -4,7 +4,10 @@
 
 import pandas as pd
 import json
-
+import base64
+import os
+from PIL import Image
+import html
 
 TUNA_STIMULI_IDS = [
     642,
@@ -73,6 +76,7 @@ THREEDS_STIMULI_IDS = [
 
 # Load data
 header = [
+    "set",
     "stimuli_id",
     "target",
     "D1",
@@ -89,9 +93,16 @@ header = [
 def load_raw_stimuli_info(data_set_name, stimuli_id):
     """Will return t, D1-D3, id_type, (minimal expression)"""
     if data_set_name == "tuna":
-        with open("games/multimodal_referencegame/prep/tuna/tuna_3_distractor_stimuli.json", "r") as f:
+        with open(
+            "games/multimodal_referencegame/prep/tuna/tuna_3_distractor_stimuli.json",
+            "r",
+        ) as f:
             data = json.load(f)
-            all_stimuli = data["INSTANCES"]["one_attibute_id"] + data["INSTANCES"]["two_attibute_id"] + data["INSTANCES"]["three_attibute_id"]
+            all_stimuli = (
+                data["INSTANCES"]["one_attibute_id"]
+                + data["INSTANCES"]["two_attibute_id"]
+                + data["INSTANCES"]["three_attibute_id"]
+            )
             for stimuli in all_stimuli:
                 if stimuli["stimuli_id"] == stimuli_id:
                     return (
@@ -103,9 +114,16 @@ def load_raw_stimuli_info(data_set_name, stimuli_id):
                         stimuli["id_attributes"],
                     )
     if data_set_name == "3ds":
-        with open("games/multimodal_referencegame/prep/3DS/3ds_3_distractor_instances.json", "r") as f:
+        with open(
+            "games/multimodal_referencegame/prep/3DS/3ds_3_distractor_instances.json",
+            "r",
+        ) as f:
             data = json.load(f)
-            all_stimuli = data["INSTANCES"]["one_attibute_id"] + data["INSTANCES"]["two_attibute_id"] + data["INSTANCES"]["three_attibute_id"]
+            all_stimuli = (
+                data["INSTANCES"]["one_attibute_id"]
+                + data["INSTANCES"]["two_attibute_id"]
+                + data["INSTANCES"]["three_attibute_id"]
+            )
             for stimuli in all_stimuli:
                 if stimuli["stimuli_id"] == stimuli_id:
                     return (
@@ -134,21 +152,23 @@ def load_ground_truth(data_set_name, target):
             return None
 
 
-def load_human_expression(experiment_name, stimuli_id):
+def load_human_expression(data_set_name, stimuli_id):
     """Will return all human expressions from slurk_logs for a given stimuli_id.
     Different expressions will be separated by a $.
     """
-    with open("games/multimodal_referencegame/analysis/successful_human_expressions.json", "r") as f:
+    with open(
+        "games/multimodal_referencegame/analysis/successful_human_expressions.json", "r"
+    ) as f:
         all_human_expressions = json.load(f)
     expressions = ""
-    if experiment_name == "tuna":
+    if data_set_name == "tuna":
         for game in all_human_expressions["tuna"]:
             if game["stimuli_id"] == stimuli_id:
                 expressions += "$" + game["human_expression"]
         for game in all_human_expressions["mixed"]:
             if game["stimuli_id"] == stimuli_id:
                 expressions += "$" + game["human_expression"]
-    if experiment_name == "3ds":
+    if data_set_name == "3ds":
         for game in all_human_expressions["3ds"]:
             if game["stimuli_id"] == stimuli_id:
                 expressions += "$" + game["human_expression"]
@@ -158,9 +178,20 @@ def load_human_expression(experiment_name, stimuli_id):
     return expressions.strip("$")
 
 
-def load_model_expressions(stimuli_id):
-    """Will return model_expressions"""
-    return None
+def load_model_expressions(data_set_name, stimuli_id):
+    model_expressions = json.load(
+        open("games/multimodal_referencegame/analysis/all_model_expressions.json")
+    )
+    if data_set_name == "tuna":
+        try:
+            return model_expressions["tuna"][str(stimuli_id)]
+        except KeyError:
+            return None
+    if data_set_name == "3ds":
+        try:
+            return model_expressions["threeds"][str(stimuli_id)]
+        except KeyError:
+            return None
 
 
 def write_info_table():
@@ -170,8 +201,9 @@ def write_info_table():
         t, D1, D2, D3, id_type, minimal_expression = load_raw_stimuli_info("tuna", id)
         ground_truth = load_ground_truth("tuna", t)
         human_expression = load_human_expression("tuna", id)
-        model_expressions = load_model_expressions(id)
+        model_expressions = load_model_expressions("tuna", id)
         tuna_data.loc[len(tuna_data)] = [
+            "tuna",
             id,
             t,
             D1,
@@ -187,8 +219,9 @@ def write_info_table():
         t, D1, D2, D3, id_type, minimal_expression = load_raw_stimuli_info("3ds", id)
         ground_truth = load_ground_truth("3ds", t)
         human_expression = load_human_expression("3ds", id)
-        model_expressions = load_model_expressions(id)
+        model_expressions = load_model_expressions("3ds", id)
         three_ds_data.loc[len(three_ds_data)] = [
+            "3ds",
             id,
             t,
             D1,
@@ -202,9 +235,81 @@ def write_info_table():
         ]
     complete_data = pd.concat([tuna_data, three_ds_data])
 
-    tuna_data.to_csv("games/multimodal_referencegame/analysis/tuna_info_table.csv", index=False)
-    three_ds_data.to_csv("games/multimodal_referencegame/analysis/three_ds_info_table.csv", index=False)
-    complete_data.to_csv("games/multimodal_referencegame/analysis/info_table.csv", index=False)
+    tuna_data.to_csv(
+        "games/multimodal_referencegame/analysis/tuna_info_table.csv", index=False
+    )
+    three_ds_data.to_csv(
+        "games/multimodal_referencegame/analysis/three_ds_info_table.csv", index=False
+    )
+    complete_data.to_csv(
+        "games/multimodal_referencegame/analysis/info_table.csv", index=False
+    )
+
+
+def make_html_table():
+    info_table = pd.read_csv("games/multimodal_referencegame/analysis/info_table.csv")
+    print(info_table)
+    image_columns = ["target", "D1", "D2", "D3"]
+    description_column = "human_expression"
+    for col in image_columns:
+        info_table[col] = info_table.apply(
+            lambda row: image_loader(row, row[col]), axis=1
+        )
+    info_table[description_column] = info_table[description_column].str.replace("ü", "&uuml;")
+    info_table[description_column] = info_table[description_column].str.replace("ä", "&auml;")
+    info_table[description_column] = info_table[description_column].str.replace("ö", "&ouml;")
+    info_table[description_column] = info_table[description_column].str.replace("ß", "&szlig;")
+
+    table_html = info_table.to_html(escape=False, index=False)
+    with open("games/multimodal_referencegame/analysis/experiment_overview.html", "w") as f:
+        f.write(
+            """
+        <html>
+        <head>
+            <title>Image Table</title>
+            <style>
+                table { border-collapse: collapse; width: 100%; }
+                th, td { border: 1px solid black; padding: 8px; text-align: center; }
+                img { max-height: 50px; }
+            </style>
+        </head>
+        <body>
+            <h2>Experiment Overview</h2>
+            """
+            + table_html
+            + """
+        </body>
+        </html>"""
+        )
+
+
+def image_loader(row, filename):
+    small_image = (254, 254)
+    large_size = 50
+    small_size = 30
+    if pd.notna(filename):
+        base_path = (
+            "games/multimodal_referencegame/resources/tuna_images/"
+            if row["set"] == "tuna"
+            else "games/multimodal_referencegame/resources/3ds_images/"
+        )
+        image_path = os.path.join(base_path, filename)
+        try:
+            with open(image_path, "rb") as img_file:
+                encoded_string = base64.b64encode(img_file.read()).decode("utf-8")
+            with Image.open(image_path) as img:
+                width, height = img.size
+                display_width = (
+                    small_size
+                    if width <= small_image[0] and height <= small_image[1]
+                    else large_size
+                )
+            return f'<img src="data:image/png;base64,{encoded_string}" alt="{filename}" width="{display_width}">'
+        except FileNotFoundError:
+            return filename + " not found"
+    return ""
+
 
 if __name__ == "__main__":
     write_info_table()
+    make_html_table()
