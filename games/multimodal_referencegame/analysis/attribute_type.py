f20912304902883d435985c730c2969c031761f9
Eileen <eniedenfuehr@gmail.com>
2025-02-26 18:56:10 +0100

    remove + import constants

--- games/multimodal_referencegame/analysis/attribute_type.py
+++ games/multimodal_referencegame/analysis/attribute_type.py
@@ -2,24 +2,17 @@
 # Author: Eileen Kammel, 811770
 # Date: 2025-02-25 20:24:56
 
-# Determine if some attributes make id harder
+# Determine if some attributes make id harder.
+# For each model, calculate the ratio of correct answers for each attribute.
+# Save the results in a csv file.
+
 import json
 import pandas as pd
-
-COMMERCIAL_MODELS = [
-    "gpt-4o-2024-08-06",
-    "claude-3-5-sonnet-20240620",
-    "gemini-2.0-flash-exp",
-]
-
-OPEN_WEIGHED_MODELS = [
-    "idefics-80b-instruct",
-    "InternVL2-Llama3-76B",
-    "InternVL2-40B",
-    "InternVL2-8B",
-]
-
-ALL_MODELS = COMMERCIAL_MODELS + OPEN_WEIGHED_MODELS
+from games.multimodal_referencegame.analysis.constants import (
+    ALL_MODELS,
+    TUNA_STIMULI_IDS,
+    THREEDS_STIMULI_IDS,
+)
 
 TUNA_ATTRIBUTES = ["colour", "type", "size", "orientation"]
 
@@ -30,71 +23,6 @@ THREEDS_ATTRIBUTES = [
     "wallHue",
     "objectHue",
     "orientation",
-]
-
-TUNA_STIMULI_IDS = [
-    642,
-    580,
-    965,
-    454,
-    9,
-    588,
-    975,
-    719,
-    784,
-    593,
-    977,
-    55,
-    1047,
-    793,
-    220,
-    799,
-    96,
-    675,
-    932,
-    101,
-    678,
-    1060,
-    164,
-    878,
-    307,
-    244,
-    501,
-    567,
-    123,
-    127,
-]  # List of stimuli ids for TUNA
-THREEDS_STIMULI_IDS = [
-    640,
-    1856,
-    453,
-    2693,
-    3014,
-    3589,
-    2761,
-    1224,
-    654,
-    1559,
-    3160,
-    3544,
-    3991,
-    802,
-    3367,
-    1129,
-    491,
-    684,
-    1389,
-    686,
-    2671,
-    3762,
-    3123,
-    2482,
-    2418,
-    2999,
-    3512,
-    2682,
-    1403,
-    1276,
 ]
 
 
